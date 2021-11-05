00dedc61f1c8eace6d208eb70e36a9635da47781
arios <ariostonj@hotmail.com>
2021-11-05 01:07:55 -0300

    init repository

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "./logo.svg";
+import "tailwindcss/dist/base.min.css";
+import "./App.css";
+import tw from "twin.macro";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p tw="font-extrabold text-red-500">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
